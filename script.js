c26e3802a85e598dfa35619e7f2a4d58dbb09d5e
Divya140697 <125757414+Divya140697@users.noreply.github.com>
2023-07-19 13:58:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(newAge) {
+    if (typeof newAge === "number" && newAge >= 0) {
+      this._age = newAge;
+    } else {
+      throw new Error("Age must be a non-negative number.");
+    }
+  }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this._name} is studying.`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this._name} is teaching.`);
+  }
+}
+
+// Example usage:
+/*const person1 = new Person("John", 30);
+console.log(person1.name); // Output: John
+console.log(person1.age); // Output: 30
+
+person1.age = 31;
+console.log(person1.age); // Output: 31
+
+const student1 = new Student("Alice", 25);
+console.log(student1.name); // Output: Alice
+console.log(student1.age); // Output: 25
+student1.study(); // Output: Alice is studying.
+
+const teacher1 = new Teacher("Mr. Smith", 40);
+console.log(teacher1.name); // Output: Mr. Smith
+console.log(teacher1.age); // Output: 40
+teacher1.teach(); // Output: Mr. Smith is teaching.
+*/
